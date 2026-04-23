93f2b5c6ef79f52578ce5f350cd0bf75a14eeb79
Julia Fokanova <oncologmma@mail.ru>
2026-04-23 22:26:10 +0300

    ci/cd: fix base url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_router-crud-frontend',
+  base: '/posts-router/',
   server: {
     open: true,
   },
