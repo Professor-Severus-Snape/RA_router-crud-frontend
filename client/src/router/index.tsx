88a61bd6170ea24402033e0e259b70740faddbb1
Julia Fokanova <oncologmma@mail.ru>
2026-04-24 19:20:44 +0300

    chore: rename loaders/ directory

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import pingServer from '../utils/pingServer';
+import pingServer from '../loaders/pingServer';
 
 import Layout from '../components/Layout/Layout';
 import Loader from '../components/Loader/Loader';
